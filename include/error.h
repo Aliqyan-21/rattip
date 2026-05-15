6a212cb43c65ce63e6c9dda1131dd006a34456cf
Aliqyan-21 <aliqyanabid21@gmail.com>
2026-05-15 10:17:04 +0530

    add: custom error class

---
+++ include/error.h
@@ -0,0 +1,23 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+#include <stdexcept>
+
+class RappitError : public std::exception {
+public:
+  std::string msg_;
+  std::string ctx_;
+
+  const char *what() const noexcept override;
+
+  RappitError(const std::string &msg, const std::string &ctx = "");
+
+  virtual std::string format() const;
+};
+
+class RuntimeError : public RappitError {
+public:
+  explicit RuntimeError(const std::string &msg, const std::string &ctx = "");
+};
+
+#endif  //! ERROR_H
---
+++ src/error.cpp
@@ -0,0 +1,16 @@
+#include "error.h"
+#include <sstream>
+
+std::string RappitError::format() const {
+  std::stringstream ss;
+  ss << msg_ << std::endl;
+  if (!ctx_.empty()) { ss << "  " << ctx_ << std::endl; }
+  return ss.str();
+}
+
+RappitError::RappitError(const std::string &msg, const std::string &ctx)
+  : msg_(msg), ctx_(ctx) {}
+const char *RappitError::what() const noexcept { return msg_.c_str(); }
+
+RuntimeError::RuntimeError(const std::string &msg, const std::string &ctx)
+  : RappitError("[Runtime Error] " + msg, ctx) {}
