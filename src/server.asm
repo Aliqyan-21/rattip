656c0fbab4884cc4ebc893a9e410c3807bbf7613
Aliqyan-21 <aliqyanabid21@gmail.com>
2026-05-26 21:57:50 +0530

    feat: bind

--- src/server.asm
+++ src/server.asm
@@ -10,6 +10,8 @@ serve:
 
   mov [public_dir], rdi ; rdi : public_dir
   mov [port], esi       ; esi : port
+  xchg al, ah
+  mov [sin_port], ax
   mov [reload_gen], rdx ; rdx : reload_gen
 
   mov rax, 41 ; socket()
@@ -28,6 +30,11 @@ serve:
   mov r8, 4
   syscall
 
+  mov rax, 49
+  mov rdi, [sockfd]
+  mov rsi, sockaddr_in
+  mov rdx, 16
+
   pop rbp
   ret
 
@@ -37,3 +44,8 @@ port       dd 0         ; int (4 bytes)
 reload_gen dq 0         ; int* (8 bytes)
 sockfd     dq 0         ; int*
 opt        dd 1         ; value is 1 (enable)
+sockaddr_in:
+sin_family dw 2         ; AF_INET 2 bytesr
+sin_port   dw 0         ; filled in runtime, after port arg
+sin_addr   dd 0         ; INADDR_ANY
+sin_zero   dq 0         ; padding
