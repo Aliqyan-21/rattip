344df5c6162569318da354514ba7f5c7bb190bb7
Aliqyan-21 <aliqyanabid21@gmail.com>
2026-05-26 21:43:17 +0530

    feat: socket

--- src/server.asm
+++ src/server.asm
@@ -12,10 +12,19 @@ serve:
   mov [port], esi       ; esi : port
   mov [reload_gen], rdx ; rdx : reload_gen
 
+  mov rax, 41 ; socket()
+  mov rdi, 2
+  mov rsi, 1
+  mov rdx, 0
+  syscall
+
+  mov [sockfd], rax
+
   pop rbp
   ret
 
 section '.data' writeable
 public_dir dq 0         ; char* (8 bytes)
-port dd 0               ; int (4 bytes)
+port       dd 0         ; int (4 bytes)
 reload_gen dq 0         ; int* (8 bytes)
+sockfd     dq 0         ; int*
