a3dc9de8119e0842b69f2778192bbfc93a5860ad
Aliqyan-21 <aliqyanabid21@gmail.com>
2026-05-26 21:50:33 +0530

    feat: setsockopt for port resusing

--- src/server.asm
+++ src/server.asm
@@ -20,6 +20,14 @@ serve:
 
   mov [sockfd], rax
 
+  mov rax, 54 ; ignoring the return for now, we will use it when we implement error handling later
+  mov rdi, [sockfd]
+  mov rsi, 1
+  mov rdx, 2
+  mov r10, opt
+  mov r8, 4
+  syscall
+
   pop rbp
   ret
 
@@ -28,3 +36,4 @@ public_dir dq 0         ; char* (8 bytes)
 port       dd 0         ; int (4 bytes)
 reload_gen dq 0         ; int* (8 bytes)
 sockfd     dq 0         ; int*
+opt        dd 1         ; value is 1 (enable)
